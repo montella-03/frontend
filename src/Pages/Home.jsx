250e0b79f82f63cf78f3f5dfdcd7bd4e70b6aab7
christopher <oigoc60@gmail.com>
2023-04-28 11:05:14 +0300

    rt

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Layout } from '../components'
+import { motion } from "framer-motion"
+import MediaCard from '../components/MediaCard'
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <motion.div 
+      className='h-24 w-40'
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}>
+        <MediaCard/>
+        
+       
+      </motion.div>
     </Layout>
   )
 }
